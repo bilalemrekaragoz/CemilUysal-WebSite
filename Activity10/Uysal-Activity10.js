eb9215cec8e33c257b1c6156623f1742ed8237cf
CemilUysal <16050111006@ybu.edu.tr>
2021-05-30 20:18:08 +0300

    Activity10

---
+++ Activity10/Uysal-Activity10.js
@@ -0,0 +1,33 @@
+$(function(){
+    var availableTags = [
+        "ActionScript",
+        "AppleScript",
+        "Asp",
+        "BASIC",
+        "C",
+        "C++",
+        "Clojure",
+        "COBOL",
+        "ColdFusion",
+        "Erlang",
+        "Fortran",
+        "Groovy",
+        "Haskell",
+        "Java",
+        "JavaScript",
+        "Lisp",
+        "Perl",
+        "PHP",
+        "Python",
+        "Ruby",
+        "Scala",
+        "Scheme"
+      ];
+      $( "#language" ).autocomplete({
+        source: availableTags
+      });
+
+});
+$( function() {
+    $( "#birthday" ).datepicker()
+} );
